2cecbc85571374472de2300e7ac59da460017187
Bryan-Wriggler <bryan.h.940707@gmail.com>
2025-10-25 22:11:19 -0700

    add data analysis file

---
+++ Phys 20AL/Week 4/Data Plot/data_static.py
@@ -0,0 +1,23 @@
+# Import the necessary libraries to read
+# dataset and work on that
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+mass = []
+length = [0.046,0.096,0.148,0.197,0.246,0.296,0.347,0.397,0.448,0.498]
+
+#plot the data in box value
+for n in range(1,11):
+    mass.append(n * 0.05)
+
+plt.plot(mass, length)
+plt.errorbar(mass, length,
+             yerr = 0.001,
+             fmt ='o')
+
+#plot the formula predicted values
+#l1 = np.arange(0.3,0.6,0.01)
+#plt.plot(l1, 2*np.pi*np.sqrt(l1/g))
+
+plt.savefig("static.png")
