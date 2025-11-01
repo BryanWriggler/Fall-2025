dc3c9f946e82772dc9490a96e59f2d26a43cad23
Bryan-Wriggler <bryan.h.940707@gmail.com>
2025-11-01 16:27:49 -0700

    changes to lab report 5

---
+++ Phys 20AL/Week 5/data process/data_gen_2_plot.py
@@ -0,0 +1,42 @@
+# Import the necessary libraries to read
+# dataset and work on that
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+v=[
+    [55.810,55.490,55.579,55.663,55.914,56.485],
+    [79.632,78.239,78.081,79.104,79.061,79.292]
+]
+dv=[
+    [0.033,0.032,0.032,0.032,0.032,0.033],
+    [0.047,0.045,0.045,0.046,0.046,0.046]
+]
+
+for i in range(0,2):
+    #plot data
+    plt.plot(range(1,7), v[i])
+    plt.errorbar(range(1,7), v[i],
+                yerr = dv,
+                fmt ='o')
+
+    #plot the formula predicted values
+    m,b = np.polyfit(range(1,7),v[i],deg=1) #returns slope m, intercept b
+    xseq = np.linspace(1, 6, num=100)
+    plt.plot(xseq, m* xseq + b, label = 'v=%.6f * n + %.6f'%(m,b))
+
+    #change label size
+    if i==0:
+        plt.ylim(50,60)
+    else:
+        plt.ylim(69,89)
+
+    #labels
+    plt.title("Log-Log Plot of Wave Speed (m/s) wrt Tension (N)")
+    plt.xlabel("Log(T) (N)")
+    plt.ylabel("Log(v) (m/s)")
+    plt.legend() #show labels for lines
+
+
+    plt.savefig("v_n_plot"+str(i+1)+".png")
+    plt.cla()
